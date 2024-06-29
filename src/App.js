8c413602b11cfe1ef988f39fd47ec8b66aef9992
Your GitHub Username <Your GitHub Email>
2024-06-29 19:02:28 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { ThemeProvider } from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lightTheme } from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { useState } from "react";
+import Authentication from "./pages/Authentication";
+import Favourites from "./pages/Favourites";
+import Cart from "./pages/Cart";
+import FoodDetails from "./pages/FoodDetails";
+import FoodListing from "./pages/FoodListing";
+import { useSelector } from "react-redux";
+
+const Container = styled.div``;
 
 function App() {
+  const { currentUser } = useSelector((state) => state.user);
+  const { open, message, severity } = useSelector((state) => state.snackbar);
+  const [openAuth, setOpenAuth] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Navbar
+            setOpenAuth={setOpenAuth}
+            openAuth={openAuth}
+            currentUser={currentUser}
+          />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/favorite" exact element={<Favourites />} />
+            <Route path="/cart" exact element={<Cart />} />
+            <Route path="/dishes/:id" exact element={<FoodDetails />} />
+            <Route path="/dishes" exact element={<FoodListing />} />
+          </Routes>
+          {openAuth && (
+            <Authentication setOpenAuth={setOpenAuth} openAuth={openAuth} />
+          )}
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js or App.js (or any other root component file)
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux"; // Import the Provider component
+import { store } from "./redux/store"; // Import store explicitly, assuming it's exported as named export 'store'
+import App from "./App"; // Your main App component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
